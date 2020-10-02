67ce63eef852d22eb97dcc5e99fd1d649288d86a
Musnadil <firdaus.musnadil@gmail.com>
2020-10-02 21:05:58 +0700

    belajar kedua

---
+++ src/6_kode_operasi_boolean.kt
@@ -0,0 +1,29 @@
+fun main(){
+    val finalExam = 80
+    val attendant = 75;
+
+    val passfinalExam = finalExam > 75      //true
+    val passAttendant = attendant > 80;     //false
+
+    val pass = passfinalExam && passAttendant   // true && false = false
+
+    val a = 10
+    val b = 20
+    println (pass)
+
+
+    val result1 : Boolean = a > b
+    val result2 : Boolean = a < b
+    val result3 : Boolean = a >= b
+    val result4 : Boolean = a <= b
+    println("")
+    println(result1)
+    println(result2)
+    println(result3)
+    println(result4)
+
+
+
+
+
+}
---
+++ src/7_if_expression.kt
@@ -0,0 +1,11 @@
+fun main () {
+    val nilaiUlangan = 90
+
+    if (nilaiUlangan < 91) {
+        println("kamu kurang teliti")
+    }else if (nilaiUlangan == 90){
+        println("kamu pintar")
+    }else{
+        println("kamu luar biasa")
+    }
+}
